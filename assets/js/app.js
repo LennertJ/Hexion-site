6ea554a334a74f0f95da87c237e0d3ee6ee82949
lennert <jans.lennert@gmail>
2020-09-21 12:24:44 +0200

    header v1

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,13 +4,12 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
-const $ = require('jquery');
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.css';
 import '../css/footer.css';
+import '../css/header.css';
 import '../css/homepage.scss';
 const schild = require('../images/schild.png');
-require('bootstrap');
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
