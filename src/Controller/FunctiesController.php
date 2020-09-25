48cbf70e4b3add44d2c64622038bd6ca9f185a17
lennert <jans.lennert@gmail>
2020-09-25 14:08:29 +0200

    added delete + edit functie

--- src/Controller/FunctiesController.php
+++ src/Controller/FunctiesController.php
@@ -3,11 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Functies;
+use App\Form\FunctieEditType;
 use App\Form\FunctieType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-
+use Symfony\Component\Routing\Annotation\Route;
 
 class FunctiesController extends AbstractController
 {
@@ -37,6 +38,61 @@ class FunctiesController extends AbstractController
         return $this->render('functies/add.html.twig', [
             'FunctieForm' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @Route("/admin/functie/edit{id}", name="editfunctie")
+     * @param Request $request
+     * @return Response
+     */
+    public function editfunctie(Request $request,$id):Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $functie = $entityManager->getRepository(Functies::class)->find($id);
+
+        if (!$functie) {
+            throw $this->createNotFoundException(
+                'unexpected internal error '
+            );
+        }
+
+        $form = $this->createForm(FunctieType::class, $functie);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $functie = $form->getData();
+
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($functie);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('showfuncties');
+        }
+
+        return $this->render('functies/edit.html.twig', [
+            'FunctieForm' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/admin/functie/delete{id}", name="deletefunctie")
+     * @param Request $request
+     * @return Response
+     */
+    public function deletefunctie(Request $request, $id):Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $functie = $entityManager->getRepository(Functies::class)->find($id);
+        $entityManager->remove($functie);
+        $entityManager->flush();
+
+        $functies = $this->getDoctrine()
+            ->getRepository(Functies::class)
+            ->findAll();
+
+        return $this->redirectToRoute('showfuncties');
     }
 
     /**
